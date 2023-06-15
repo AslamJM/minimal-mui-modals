019d4c39f341f5e65484ce53eac45a37f8503739
AslamJM <jma8744@gmail.com>
2023-06-15 14:02:41 +0530

    v 1.0.0

---
+++ src/component/DeleteModal.tsx
@@ -0,0 +1,30 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import { DialogContentProps } from '@mui/material/DialogContent'
+import { FC } from 'react'
+
+import { useModalContext } from './ModalContext'
+
+interface DeleteModalProps extends DialogContentProps {
+  deleteFn: () => void
+  title: string
+}
+
+const DeleteModal: FC<DeleteModalProps> = ({ children, title, deleteFn }) => {
+  const { deleteModalOpen, setDeleteModalOpen } = useModalContext()
+  return (
+    <Dialog open={deleteModalOpen}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <Button onClick={() => setDeleteModalOpen(false)} color="error" variant="outlined">
+          cancel
+        </Button>
+        <Button onClick={deleteFn} color="success" variant="outlined">
+          delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default DeleteModal
---
+++ src/component/index.ts
@@ -0,0 +1,6 @@
+import AddModal from './AddModal'
+import DeleteModal from './DeleteModal'
+import EditModal from './EditModal'
+import { ModalContextProvider, useModalContext } from './ModalContext'
+
+export { AddModal, DeleteModal, EditModal, ModalContextProvider, useModalContext }
